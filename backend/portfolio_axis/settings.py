105bd3c771b9d55cbf4f386d0a38735eedb5a5ba
nahuelxx <nahuue2@gmail.com>
2025-10-24 08:35:31 -0300

    repo update

--- backend/portfolio_axis/settings.py
+++ backend/portfolio_axis/settings.py
@@ -59,12 +59,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS_ALLOWED_ORIGINS desde .env (coma separada)
-#CORS_ALLOWED_ORIGINS = [
-#    o.strip()
-#    for o in config("CORS_ALLOWED_ORIGINS", default="http://localhost:3000").split(",")
-#    if o.strip()
-#]
+#CORS_ALLOWED_ORIGINS desde .env (coma separada)
+CORS_ALLOWED_ORIGINS = [
+    o.strip()
+    for o in config("CORS_ALLOWED_ORIGINS", default="http://localhost:3000").split(",")
+    if o.strip()
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
